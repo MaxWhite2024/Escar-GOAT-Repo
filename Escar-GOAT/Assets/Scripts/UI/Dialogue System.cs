b60d828f757b12eee9a0836cde6976390bf4a0dd
Ashank Rajendran <ashankrajendran@gmail.com>
2025-04-02 13:10:09 -0400

    Added hardcoded animation values for dialogue

--- Escar-GOAT/Assets/Scripts/UI/Dialogue System.cs
+++ Escar-GOAT/Assets/Scripts/UI/Dialogue System.cs
@@ -37,13 +37,36 @@ public class DialogueSystem : MonoBehaviour
         dialogueBox.gameObject.SetActive(true);
 
         string textToDisplay = dialogueText.text;
-        dialogueText.text = "";
+        //dialogueText.text = "";
 
         DOTweenTMPAnimator textAnimator = new DOTweenTMPAnimator(dialogueText);
 
         Sequence dialogueSequence = DOTween.Sequence();
         dialogueSequence.Append(dialogueBox.DOAnchorPos(dialogueBoxEndPos, 0.66f, false).SetEase(Ease.OutBack));
 
-        dialogueSequence.Join(dialogueText.DOText(textToDisplay, 1));
+        //Vector3 currCharOffset = textAnimator.GetCharOffset(0);
+        //dialogueSequence.Join(textAnimator.DOPunchCharOffset(0, currCharOffset + new Vector3(0, 30, 0), 0.5f));
+
+        for (int i = 0; i < textAnimator.textInfo.characterCount; ++i)
+        {
+            textAnimator.SetCharColor(i, new Color(0, 1, 1, 0));
+            textAnimator.SetCharRotation(i, new Vector3(0, 0, 270));
+            textAnimator.SetCharOffset(i, new Vector3(100, 0, 0));
+        }
+
+
+        for (int i = 0; i < textAnimator.textInfo.characterCount; ++i)
+        {
+            if (!textAnimator.textInfo.characterInfo[i].isVisible) continue;
+            Vector3 currCharOffset = textAnimator.GetCharOffset(i);
+            dialogueSequence.Insert(0.33f + i * 0.02f,
+                textAnimator.DOColorChar(i, Color.white, 0.5f));
+            dialogueSequence.Join(textAnimator.DORotateChar(i, Vector3.zero, 0.5f, RotateMode.Fast));
+            dialogueSequence.Join(textAnimator.DOOffsetChar(i, Vector3.zero, 0.5f));
+        }
+
+        dialogueSequence.AppendInterval(3);
+        dialogueSequence.Append(dialogueBox.DOAnchorPos(new Vector2(dialogueBoxEndPos.x, -dialogueBox.sizeDelta.y),
+            0.66f, false).SetEase(Ease.InBack).OnComplete(() => dialogueBox.gameObject.SetActive(false)));
     }
 }
